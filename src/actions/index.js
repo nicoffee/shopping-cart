add44de3f568efc2321011285f336173df80cc8d
nicoffee <fedoseevn@gmail.com>
2017-09-05 10:23:06 +0500

    move action types to constants

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,16 @@
 import {v4} from 'node-uuid'
 
+export const ADD_GOOD = 'ADD_GOOD'
+export const REMOVE_GOOD = 'REMOVE_GOOD'
+export const SET_CATEGORY_FILTER = 'SET_CATEGORY_FILTER'
+export const SUCCESS_REQUEST = 'SUCCESS_REQUEST'
+
+export const VisibilityFilters = {
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_CASES: 'SHOW_CASES',
+    SHOW_MONITORS: 'SHOW_MONITORS'
+}
+
 export const addGood = (img, name, vendor, price, rating) => ({
     type: 'ADD_GOOD',
     id: v4(),
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,11 +1,17 @@
+import {ADD_GOOD, REMOVE_GOOD, SUCCESS_REQUEST, SET_CATEGORY_FILTER} from './../actions'
+import {VisibilityFilters} from './../actions'
+
+const SHOW_ALL = VisibilityFilters;
+
+
 export const good = (state, action) => {
     switch (action.type) {
-        case 'ADD_GOOD':
+        case ADD_GOOD:
             return {
                 name: action.name,
                 id: action.id
             };
-        case 'REMOVE_GOOD':
+        case REMOVE_GOOD:
             return state;
         default:
             return state;
@@ -14,24 +20,23 @@ export const good = (state, action) => {
 
 export const goods = (state = [], action) => {
     switch (action.type) {
-        case 'ADD_GOOD':
+        case ADD_GOOD:
             return ([
                 ...state,
                 good(undefined, action),
             ]);
-        case 'REMOVE_GOOD':
+        case REMOVE_GOOD:
             return state;
-        case 'SUCCESS_REQUEST':
+        case SUCCESS_REQUEST:
             return action.goods;
         default:
             return state;
     }
 };
 
-export const filter = (state = 'ALL', action) => {
-    console.log('STATE', state);
-    switch (action.filter) {
-        case 'CASES':
+export const filter = (state = SHOW_ALL, action) => {
+    switch (action.type) {
+        case SET_CATEGORY_FILTER:
             return action.filter;
         default:
             return state;
