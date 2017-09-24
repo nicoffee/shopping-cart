33b4038cbeba50cde7e4eaeb4dc1a6e6e9affbaf
nicoffee <fedoseevn@gmail.com>
2017-09-24 16:52:47 +0500

    add selector

--- src/containers/VisibleGoodsList.js
+++ src/containers/VisibleGoodsList.js
@@ -2,26 +2,15 @@ import {connect} from 'react-redux'
 import {withRouter} from 'react-router'
 import {addGoodInCart} from '../actions'
 import GoodsList from '../components/GoodsList'
+import {getVisibleGoods} from '../reducers'
 import {goods} from './../../db.json'
 
-const getVisibleGoods = (goods, filter) => {
-    let allGoods = [];
-    Object.keys(goods).map(key => allGoods = allGoods.concat(goods[key]));
 
-    switch (filter) {
-        case 'SHOW_ALL':
-            return allGoods;
-        case 'SHOW_CASES':
-            return goods.cases;
-        case 'SHOW_MONITORS':
-            return goods.monitors
-    }
-};
 
 const mapStateToProps = (state, {match}) => (
     {
         goods: getVisibleGoods(
-            goods, 
+            state, 
             match.params.filter || 'SHOW_ALL'
         )
     }
