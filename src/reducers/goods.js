4b682c4020469b1a9ec87baaeb094d32fe6167c6
nicoffee <fedoseevn@gmail.com>
2017-11-11 11:42:24 +0500

    remove typescript

--- src/reducers/goods.js
+++ src/reducers/goods.js
@@ -1,61 +1,60 @@
 import {
-    ADD_GOOD, 
-    REMOVE_GOOD,
-    GOODS_REQUEST,
-    GOODS_SUCCESS,
-    GOODS_FAILURE
+  ADD_GOOD,
+  REMOVE_GOOD,
+  GOODS_REQUEST,
+  GOODS_SUCCESS,
+  GOODS_FAILURE
 } from './../types'
 
-
 export const good = (state, action) => {
-    switch (action.type) {
-        case ADD_GOOD:
-            return {
-                name: action.name,
-                id: action.id
-            };
-        case REMOVE_GOOD:
-            return state;
-        default:
-            return state;
-    }
-};
+  switch (action.type) {
+    case ADD_GOOD:
+      return {
+        name: action.name,
+        id: action.id
+      }
+    case REMOVE_GOOD:
+      return state
+    default:
+      return state
+  }
+}
 
-import {goodsFromDb} from './../../db.json'
+import { goodsFromDb } from './../../db.json'
 
-const byId = (state = {
+const byId = (
+  state = {
     isFetching: false,
     goods: []
-}, action) => {
-    switch (action.type) {
-        case ADD_GOOD:
-        case REMOVE_GOOD:
-            return {
-                ...state,
-                [action.id]: good(state[action.id], action),
-            };
-        default:
-            return state;
-    }
-};
+  },
+  action
+) => {
+  switch (action.type) {
+    case ADD_GOOD:
+    case REMOVE_GOOD:
+      return {
+        ...state,
+        [action.id]: good(state[action.id], action)
+      }
+    default:
+      return state
+  }
+}
 
-const allIds = (state = [], action) => {
+const allIds = (state = [], action) => {}
 
-    }   
-};
-
-export default goods;
+export default allIds
 
 export const getVisibleGoods = (state, filter) => {
-    let allGoods = [];
-    Object.keys(state).map(key => allGoods = allGoods.concat(state[key]));
+  let allGoods = []
+  Object.keys(state).map(key => (allGoods = allGoods.concat(state[key])))
 
-    switch (filter) {
-        case 'all':
-            return allGoods;
-        case 'cases':
-            return state.cases;
-        case 'monitors':
-            return state.monitors
-    }
-};
+  switch (filter) {
+    case 'all':
+      return allGoods
+    case 'cases':
+      return state.cases
+    case 'monitors':
+      return state.monitors
+  }
+}
