303d6edb62f9c11e6b11e84b2e140bb2584ea273
nicoffee <fedoseevn@gmail.com>
2017-12-02 14:25:40 +0500

    rename all files extension to jsx

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import Button from 'material-ui/Button'
 import FilterLink from './../../containers/FilterLink'
 
 const Sidebar = () => (
-    <div>
-        <FilterLink filter="SHOW_ALL"><Button>Show All</Button></FilterLink>
-        <FilterLink filter="SHOW_MONITORS"><Button>Show Monitors</Button></FilterLink>
-        <FilterLink filter="SHOW_CASES"><Button>Show Cases</Button></FilterLink>
-    </div>
-);
+  <div>
+    <FilterLink filter="SHOW_ALL">
+      <Button>Show All</Button>
+    </FilterLink>
+    <FilterLink filter="SHOW_MONITORS">
+      <Button>Show Monitors</Button>
+    </FilterLink>
+    <FilterLink filter="SHOW_CASES">
+      <Button>Show Cases</Button>
+    </FilterLink>
+  </div>
+)
 
-const mapStateToProps = (state) => ({state});
+const mapStateToProps = state => ({ state })
 
 export default connect(mapStateToProps)(Sidebar)
