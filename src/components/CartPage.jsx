2ab1b352df38c44a2c43d44dda594839b759b0bf
nicoffee <fedoseevn@gmail.com>
2018-01-05 11:53:58 +0500

    Add message about no items

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -4,13 +4,20 @@ import PropTypes from 'prop-types'
 const CartPage = ({ allIds, byId, price }) => (
   <div>
     <h1>Cart</h1>
-    <ul>{allIds.map((id, idx) => <li key={idx}>{byId[id].name}</li>)}</ul>
-    <h2>Price: ${price}</h2>
+    {allIds.length ? (
+      <div>
+        <ul>{allIds.map((id, idx) => <li key={idx}>{byId[id].name}</li>)}</ul>
+        <h2>Price: ${price}</h2>
+      </div>
+    ) : (
+      <div>No items in cart</div>
+    )}
   </div>
 )
 
 CartPage.propTypes = {
-  goodsInCart: PropTypes.object,
+  allIds: PropTypes.array,
+  byId: PropTypes.object,
   price: PropTypes.number
 }
 
