01c732494402c32d3a2f8cb38a31b922ddf2cf73
nicoffee <fedoseevn@gmail.com>
2018-01-16 10:02:27 +0500

    Add tests

--- src/__tests__/Good.test.jsx
+++ src/__tests__/Good.test.jsx
@@ -1,22 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import Enzyme, { shallow, mount } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import Card from "material-ui/Card";
-import Good from "./../components/Good";
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
+import Good from './../components/Good';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-// test('It renders <Good /> component', () => {
-//   let props
-//   let mountedGood
-
-//   mountedGood = shallow(<Good {...props} onClick={() => {}} />)
-
-//   expect(mountedGood.length).toBeGreaterThan(0)
-// })
-
-describe("LockScreen", () => {
+describe('LockScreen', () => {
   let props;
   let mountedGood;
   const good = () => {
@@ -43,7 +34,33 @@ describe("LockScreen", () => {
     mountedGood = undefined;
   });
 
-  it("always renders a `Card`", () => {
+  it('always renders a `Card`', () => {
     expect(good().find(Card).length).toBe(1);
+  });
+
+  it('always renders a `CardMedia`', () => {
+    expect(good().find(CardMedia).length).toBe(1);
+  });
+
+  it('always renders a `CardContent`', () => {
+    expect(good().find(CardContent).length).toBe(1);
+  });
+
+  it('always renders a `CardActions`', () => {
+    expect(good().find(CardActions).length).toBe(1);
+  });
+
+  describe('rendered `CardContent`', () => {
+    it('does not receive any props', () => {
+      const CardContent = good().find(CardContent);
+      expect(Object.keys(CardContent.props()).length).toBe(0);
+    });
+  });
+
+  describe('rendered `CardActions`', () => {
+    it('does not receive any props', () => {
+      const CardActions = good().find(CardActions);
+      expect(Object.keys(CardActions.props()).length).toBe(0);
+    });
   });
 });
