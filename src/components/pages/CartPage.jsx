864fc3c6bf9dedc1e1fc2d4d1fe82826c055aa36
nicoffee <fedoseevn@gmail.com>
2018-01-21 01:08:24 +0500

    Add table head

--- src/components/pages/CartPage.jsx
+++ src/components/pages/CartPage.jsx
@@ -1,38 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CartPage = props => {
-  console.log('props', props);
-  const { allIds, byId, price } = props;
-  return (
-    <div>
-      <h1>Cart</h1>
-      {allIds.length ? (
-        <div>
-          <table>
-            <tbody>
-              {allIds.map((id, idx) => (
-                <tr key={idx}>
-                  <td>
-                    <img src={byId[id].img} alt="" />
-                  </td>
-                  <td>{byId[id].name}</td>
-                  <td>{byId[id].price}</td>
-                  <td>
-                    <input type="text" />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <h2>Price: ${price}</h2>
-        </div>
-      ) : (
-        <div>No items in cart</div>
-      )}
-    </div>
-  );
-};
+const CartPage = ({ allIds, byId, price }) => (
+  <div>
+    <h1>Cart</h1>
+    {allIds.length ? (
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <th>Photo</th>
+              <th>Product name</th>
+              <th>Price per item</th>
+              <th>Amount</th>
+            </tr>
+            {allIds.map((id, idx) => (
+              <tr key={idx}>
+                <td>
+                  <img src={byId[id].img} alt="" />
+                </td>
+                <td>{byId[id].name}</td>
+                <td>{byId[id].price}</td>
+                <td>
+                  <input type="text" />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <h2>Price: ${price}</h2>
+      </div>
+    ) : (
+      <div>No items in cart</div>
+    )}
+  </div>
+);
 
 CartPage.propTypes = {
   allIds: PropTypes.array,
