755269f5c91ea4a2259a310419a2fac6c6f47528
Nikolay Fedoseev <n.fedoseev@xsolla.com>
2018-03-28 18:56:52 +0500

    Update server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+console.log('Hello world!');
+
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('*', (req, res) => {
